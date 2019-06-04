6f6a910213d0834c169e79061c3b0ad73ce42975
Sergej Razgonov <ssrazgonov@gmail.com>
2019-06-04 23:34:58 +0300

    add first logic

--- inception-way/src/app.js
+++ inception-way/src/app.js
@@ -1,9 +1,47 @@
 $(document).ready(function(){
-    //start intro
-
-    var introSub = $('.intro-sub');
     
-    introSub.addClass('intro-play');
+    let state = localStorage.getItem('state') ? localStorage.getItem('state') : "intro";
 
+    saveState(state);
+
+    showState(state);
+
+    function showState (state) {
+        switch (state) {
+            case 'intro':
+                intro ();
+                break;
+            case 'day1':
+                day1();
+                break;
+            default:
+                break;
+        }
+    }
+
+    function intro () {
+        $('.intro').show();
+        $('.introBtn').on('click', function(){
+            nextState('.intro', 'day1');
+        });
+    }
+
+    function day1 () {
+        $('.day1').show();
+    }
+
+    function nextState (currState, nextState) {
+        closeState(currState);
+        showState(nextState);
+        saveState(nextState);
+    }
+
+    function saveState (sate) {
+        // localStorage.setItem('state', state);
+    }
+
+    function closeState (state) {
+        $(state).hide();
+    }
 
 });
