6f86d48904432de1b78ffdd3e32c7fdcf9b64d1d
Sergej Razgonov <ssrazgonov@gmail.com>
2019-06-05 00:57:39 +0300

    Add some day 1 functions

--- inception-way/src/app.js
+++ inception-way/src/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     
-    let state = localStorage.getItem('state') ? localStorage.getItem('state') : "intro";
+    let state = localStorage.getItem('state') ? localStorage.getItem('state') : "day1";
 
     saveState(state);
 
@@ -27,7 +27,50 @@ $(document).ready(function(){
     }
 
     function day1 () {
+
+        let find = false;
+        let nextPhase = function () {
+            alert('NEXT PHASE');
+        };
+
         $('.day1').show();
+        $('.chooseClearSignal').on('click', function(){
+            $('.chooseClearSignalUl').show();
+        });
+
+        $('.chooseClear1').on('click', function(){
+            let text = $('.terminal').text();
+            text = text.replace(/#/, '');
+            $('.terminal').text(text);
+        });
+
+        $('.chooseClear2').on('click', function(){
+            let text = $('.terminal').text();
+            text = text.replace(/%/, '');
+            $('.terminal').text(text);
+        });
+
+        $('.chooseInputSignal').on('click', function(){
+            if (find) {
+                return false;
+            }
+
+            if ($('.terminal').text().trim() == 'Частота 187.535') {
+                $(this).text('Сигнал найден');
+                find = true;
+                setTimeout(function(){
+                    nextPhase();
+                }, 500);
+            }else {
+                
+                $(this).text('Частота не найдена');
+
+                setTimeout(function(){
+                    $('.chooseInputSignal').text('Ввести сигнал в поиск');
+                }, 500);
+            }
+            return false;
+        });
     }
 
     function nextState (currState, nextState) {
